723cfb51a64363e22922b90b46be207c853daac1
dinhtri123 <88138898+dinhtri123@users.noreply.github.com>
2021-08-12 21:18:38 +0700

    update

---
+++ Scroll.js
@@ -0,0 +1,106 @@
+const sr = ScrollReveal(
+    {
+        duration: 1500,
+        reset : true
+    }
+);
+     sr.reveal('.container',{
+        origin: 'top',
+        distance: '50px',
+     })
+     sr.reveal('.logo',{
+        origin: 'top',
+        distance: '50px',
+        delay:500,
+        scale:0.9
+     })
+     sr.reveal('.cart',{
+        origin: 'top',
+        distance: '50px',
+        delay:500,
+        scale:0.9
+     })
+     sr.reveal('.menu-icon',{
+        origin: 'top',
+        distance: '50px',
+        delay:500,
+        scale:0.9
+     })
+     sr.reveal('.banner-title',{
+        origin: 'top',
+        distance: '100px',
+        delay:1000,
+        scale:0.9
+     })
+     sr.reveal('.banner-slogan',{
+        origin: 'top',
+        distance: '100px',
+        delay:1000,
+        scale:0.9
+     })
+     sr.reveal('.btn',{
+        origin: 'bottom',
+        distance: '40px',
+        delay:1000,
+        scale:0.9
+     })
+     sr.reveal('.logo-banner',{
+        origin: 'top',
+        distance: '100px',
+        delay:1000,
+        scale:0.9
+     })
+     sr.reveal('.catetory',{
+        origin: 'top',
+        scale:0.9
+     })
+     sr.reveal('.title',{
+        origin: 'top',
+        distance: '100px',
+        scale:0.9
+     })
+     sr.reveal('.row',{
+        origin: 'top',
+        distance: '100px',
+        scale:0.9
+     })
+     sr.reveal('.col-4',{
+        origin: 'top',
+        distance: '100px',
+        scale:0.9
+     })
+    //  sr.reveal('.btn_more',{
+    //     origin: 'top',
+    //     distance: '100px',
+    //     scale:0.9
+    //  })
+    sr.reveal('.page-btn',{
+        origin: 'top',
+        distance: '100px',
+        scale:0.9
+     })
+     sr.reveal('table',{
+        origin: 'top',
+        distance: '100px',
+        scale:0.9
+     })
+     sr.reveal('.about',{
+      origin: 'top',
+      distance: '100px',
+      scale:0.9
+   })
+   sr.reveal('.contact',{
+      origin: 'top',
+      distance: '100px',
+      scale:0.9
+   })
+   sr.reveal('.contact_infor',{
+      origin: 'left',
+      distance: '300px',
+      scale:0.8,
+   })
+   sr.reveal('iframe',{
+      origin: 'top',
+      distance: '300px',
+      scale:0.8,
+   })
--- js.js
+++ js.js
@@ -1,24 +1,36 @@
 //   -------js for MenuToggle------------------
 
-
   
   var  MenuItems = document.getElementById('MenuItems');
 
-  MenuItems.style.maxHeight = "0px";
+  MenuItems.style.maxHeight = "0%";
 
   function menutoggle(){
 
-      if(MenuItems.style.maxHeight == "0px"){
+      if(MenuItems.style.maxHeight == "0%"){
 
-        MenuItems.style.maxHeight = "200px";
+        MenuItems.style.maxHeight = "100%";
       }
       else{
 
-        MenuItems.style.maxHeight = "0px";
+        MenuItems.style.maxHeight = "0%";
       }
 
   }
-//   ----------------------js for product-----------------
+  // function close_menu(){
+  //   document.getElementById('MenuItems_close').style.display = "none";
+  // }
+//   function close_menu(){
+//     if(MenuItems.style.maxWidth == "100%"){
+//       MenuItems.style.maxWidth = "0%";
+//     }
+//     else{
+//       MenuItems.style.maxWidth = "100%";
+//     }
+//   }
+// //   ----------------------js for product-----------------
+
+
 
     var ProductImg = document.getElementById("ProductImg")
     var SmallImg = document.getElementsByClassName("small-img")
@@ -39,4 +51,8 @@
     {
         ProductImg.src = SmallImg[3].src
     }
-    // -----------------js for toggle form--------------
+  
+
+    // ----scroll----
+    
+ 
